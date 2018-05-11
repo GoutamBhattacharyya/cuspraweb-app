8b4d4722fbd03fa79c123c9d0e01f1b67468eaeb
goutam bhattacharyya <goutamb@coachlist.com>
2018-05-11 21:13:05 +0530

    New upload

---
+++ src/js/custom.js
@@ -0,0 +1,244 @@
+//Product para more than 145 px height will hede and when user click on down arrow it will show rest text.
+function productMore() {
+    $(".more").each(function () {
+        var myHeight = $(this).find(".cp-product-content").height();
+        console.log(myHeight);
+        if (myHeight < 145) {
+            $(this).find(".more-anchor").hide();
+        } else {
+            $(this).css({
+                "height": "146px"
+            });
+        }
+    });
+}
+
+// function for people currently serching software solution
+
+var searchItems = [
+    "Revenue Leakage Analysis in Healthcare industry",
+    "Churn Management in Telecom Industry",
+    "Lorem ipsum dolor sit amet",
+    "consectetur adipiscing elit",
+    "Eu hendrerit lorem scelerisque",
+    "Aenean ornare lectus sed ex luctus consectetur"
+];
+var arryData = 0;
+
+function updateCurrentSearch() {
+    var firstData;
+    var secondData;
+    if (searchItems[arryData] != "undefined") {
+        firstData = searchItems[arryData];
+    } else {
+        firstData = "";
+    }
+    console.log(searchItems[arryData + 1]);
+    if (searchItems[arryData + 1] === undefined) {
+        secondData = "";
+    } else {
+        secondData = searchItems[arryData + 1];
+    }
+    if (secondData == "") {
+        $(".search-text li").first().find("a").text(firstData);
+        $(".search-text li").last().find("a").text(secondData);
+    } else {
+        $(".search-text li").first().find("a").text(firstData + ", ");
+        $(".search-text li").last().find("a").text(secondData + "... ");
+    }
+    arryData += 1;
+    arryData = arryData + 1;
+    if (arryData >= searchItems.length) {
+        arryData = 0;
+    }
+
+}
+
+var winWidth = $(window).width();
+$(document).ready(function () {
+    //productMore function will work on only from 756 px screen resolution.
+
+    if (winWidth < 768) {
+        productMore();
+    }
+
+    // Function for autocomplete search field
+    $(function () {
+        var availableTags = [
+            "Customer Advocacy (15)",
+            "Customer Experience (17)",
+            "Customer Engagement (9)",
+            "Customer Interaction (11)",
+            "Customer Satisfaction (21)",
+            "Customer Service (67)",
+            "Customer Success (35)",
+            "Customer Advocacy (15)",
+            "Customer Experience (17)",
+            "Customer Engagement (9)",
+            "Customer Interaction (11)",
+            "Customer Satisfaction (21)",
+            "Customer Service (67)",
+            "Customer Success (35)",
+            "Customer Advocacy (15)",
+            "Customer Experience (17)",
+            "Customer Engagement (9)",
+            "Customer Interaction (11)",
+            "Customer Satisfaction (21)",
+            "Customer Service (67)",
+            "Customer Success (35)"
+        ];
+        $(".cp-bpfield").autocomplete({
+            source: availableTags
+        });
+    });
+
+    // Function for search industry
+    $(function () {
+        $("#cp-industry").chosen({
+            placeholder_text_single: "Industry *"
+        });
+    });
+
+
+    // Click function for mobile menu
+    $('#nav-icon').click(function () {
+        $(this).toggleClass('open');
+        // $('body').toggleClass('move-body-right');
+        // $(".main-nav").toggleClass('move-right-zero');
+        if ($("body").hasClass("moved-right")) {
+            $(".main-nav").animate({
+                "right": "-254px"
+            });
+            $(".sticky-header").animate({
+                "left": "0px",
+            });
+            $("body").animate({
+                "right": "0px"
+            }, function () {
+                $('body').css({
+                    "position": "inherit",
+                    "overflow": "visible"
+                }).removeClass("moved-right");
+            });
+        } else {
+            $(".sticky-header").animate({
+                "left": "-254px"
+            });
+            $('body').css({
+                    "position": "absolute",
+                    "overflow": "hidden",
+                    "width": "100%"
+                })
+                .animate({
+                    "right": "254px"
+                })
+                .addClass("moved-right");
+            $(".main-nav").animate({
+                "right": "0px"
+            });
+        }
+    });
+
+    //initialize first two search array valy after document load
+
+    $(".search-text li").first().find("a").text(searchItems[0] + ", ");
+    $(".search-text li").last().find("a").text(searchItems[1] + "... ");
+
+    //Smooth scroll function after clicking menu anchor
+    (function ($) {
+        "use strict"; // Start of use strict
+
+        // Smooth scrolling using jQuery easing
+        $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    if ($(window).width() < 768) {
+                        $("header").stop().delay(300).animate({
+                            "left": "-254px"
+                        });
+                    }
+                    $('html, body').animate({
+                        scrollTop: (target.offset().top - 80)
+                    }, 1000, "easeInOutExpo", function () {
+                        // if ($(window).width() < 768) {
+                        //     $('#nav-icon').removeClass('open');
+                        //     $(".main-nav").animate({
+                        //         "right": "-254px"
+                        //     });
+                        //     $(".sticky-header").animate({
+                        //         "left": "0px",
+                        //     });
+                        //     $("body").animate({
+                        //         "right": "0px"
+                        //     }, function () {
+                        //         $('body').css({
+                        //             "position": "inherit",
+                        //             "overflow": "visible"
+                        //         }).removeClass("moved-right");
+                        //     });
+                        // }
+                    });
+                    return false;
+                }
+            }
+        });
+
+        // Closes responsive menu when a scroll trigger link is clicked
+        $('.js-scroll-trigger').click(function () {
+            $('.navbar-collapse').collapse('hide');
+        });
+
+        // Activate scrollspy to add active class to navbar items on scroll
+        $('body').scrollspy({
+            target: '#mainNav',
+            offset: 80
+        });
+
+    })(jQuery); // End of use strict
+
+});
+$(window).resize(function () {
+    //productMore function will work on only from 756 px screen resolution.
+    winWidth = $(window).width();
+    $(".more").removeClass("less");
+    if (winWidth < 768) {
+        productMore();
+    } else {
+        $(".more").css("height", "auto").removeClass("less");
+    }
+});
+
+
+$(".more-anchor").click(function (e) {
+    // Show hide extra content
+    var myHeight = $(this).parent().find(".cp-product-content").height();
+    console.log("a");
+    e.preventDefault();
+    if ($(this).parent().hasClass("less")) {
+        $(this).parent().animate({
+            height: 146
+        }).removeClass("less");
+    } else {
+        $(this).parent().animate({
+            height: myHeight + 15
+        }).addClass("less");
+    }
+});
+
+$(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 80) {
+        $("header").addClass("sticky-header");
+    } else {
+        $("header").removeClass("sticky-header");
+    }
+});
+
+
+window.setInterval(function () {
+    /// call your function here
+    updateCurrentSearch();
+}, 3000);
